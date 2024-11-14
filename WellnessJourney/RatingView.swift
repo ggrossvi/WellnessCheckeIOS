efa702f5708fa3edcd59af24d7b24e3a3f9ff637
Gloria Villa <hello.gloria@gmail.com>
2024-11-14 11:55:56 -0800

    first commit

---
+++ WellnessJourney/RatingView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingView.swift
+//  WellnessJourney
+//
+//  Created by Gloria Villa on 11/13/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}
