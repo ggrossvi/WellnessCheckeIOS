4545e6701b07777fea03b512ba5fce4489105e68
Gloria Villa <hello.gloria@gmail.com>
2024-11-13 16:14:39 -0800

    Initial Commit

---
+++ WellnessJourney/WellnessJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessJourneyApp.swift
+//  WellnessJourney
+//
+//  Created by Gloria Villa on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WellnessJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
