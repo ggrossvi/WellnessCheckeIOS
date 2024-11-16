4134f8755f94edbd25744cd10e51ead9ceace9f5
Gloria Villa <hello.gloria@gmail.com>
2024-11-16 00:59:34 -0800

    added emoji feature

---
+++ JournalView.swift
@@ -0,0 +1,20 @@
+//
+//  JournalView.swift
+//  WellnessJourney
+//
+//  Created by Gloria Villa on 11/16/24.
+//
+
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JournalView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalView()
+    }
+}
